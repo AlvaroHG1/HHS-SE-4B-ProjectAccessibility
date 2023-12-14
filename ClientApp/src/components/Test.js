5ea6833535e93d84692343bc50abbecc10cf284c
timva <22102043@student.hhs.nl>
2023-12-14 16:25:19 +0100

    Added swagger to the project

--- ClientApp/src/components/Test.js
+++ ClientApp/src/components/Test.js
@@ -1,7 +1,10 @@
 import {Component} from "react";
 
 export class Test extends Component {
-    render() {return(
+    render() 
+    {
+        return(
 <h1>Test...</h1>
+            
     )}
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ProjectAccessibility.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AccessibilityContext>(opt => opt.UseInMemoryDatabase("AccessibilityDB"));
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -12,6 +19,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapControllers();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
