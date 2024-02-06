3e9d9ed50ba73fb7a65091e460b60694a38e922d
Sahel <58035719+itssahel02@users.noreply.github.com>
2024-02-06 10:12:31 +0100

    ficed error in Onderzoektest

--- Models/RequestModels/OnderzoekRequestModel.cs
+++ Models/RequestModels/OnderzoekRequestModel.cs
@@ -8,8 +8,8 @@ public class OnderzoekRequestModel
     public String Titel { get; set; }
     public String Beschrijving { get; set; }
     public String Locatie { get; set; }
-    public DateTime Startdatum { get; set; }
-    public DateTime Einddatum { get; set; }
+    public DateOnly Startdatum { get; set; }
+    public DateOnly Einddatum { get; set; }
     public String GezochteBeperking { get; set; }
     public int GezochtePostcode { get; set; }
     public int MinLeeftijd { get; set; }
